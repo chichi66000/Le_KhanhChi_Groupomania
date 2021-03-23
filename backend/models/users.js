0521772572155a28ecc8b72c301af440cc7bfbef
Le Chi <le.khanhchi@yahoo.fr>
2021-03-23 12:08:51 +0100

    reussit à poster 1 user

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,70 +1,234 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Users extends Model {
-    /**
+// const {
+//   Model
+// } = require('sequelize');
+const db = require('../server'); // fichier où se trouve l'instance sequelize
+// const { Sequelize, DataTypes, Model } = require('sequelize');
+// const sequelize = new Sequelize();
+
+// class User extends Model {
+  /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
-      // define association here
-    }
-  };
-  Users.init({
-    id: {
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      type: DataTypes.INTEGER
-    },
-    nom: {
-      allowNull: false,
-      type: DataTypes.STRING
-    },
-    prenom: {
-      allowNull: false,
-      type: DataTypes.STRING
-    },
-    email: {
-      allowNull: false,
-      type: DataTypes.STRING,
-      unique: "email"
-    },
-    password: {
-      allowNull: false,
-      type: DataTypes.STRING
-    },
-    fonction: {
-      allowNull: true,
-      type: DataTypes.STRING
-    },
-    pseudo: {
-      allowNull: false,
-      type: DataTypes.STRING,
-      unique: "pseudo"
-    },
-    avatar: {
-      allowNull: true,
-      type: DataTypes.STRING
-    },
-    isAdmin: {
-      allowNull: false,
-      type: DataTypes.BOOLEAN
-    },
-    createdAt: {
-      allowNull: false,
-      type: DataTypes.DATE
-    },
-    updatedAt: {
-      allowNull: false,
-      type: DataTypes.DATE
-    }
-  }, {
-    sequelize,
-    modelName: 'Users',
-  });
-  return Users;
-};
+  //  static associate(models) {
+    // define association here
+    // }
+// }
+
+const Sequelize = require('sequelize');
+module.exports = function (sequelize, DataTypes) {
+    const users = sequelize.define(
+      "Users", {
+        id: {
+          autoIncrement: true,
+          type: DataTypes.INTEGER,
+          allowNull: false,
+          primaryKey: true,
+        },
+        nom: {
+          allowNull: false,
+          type: DataTypes.STRING
+        },
+        prenom: {
+          allowNull: false,
+          type: DataTypes.STRING
+        },
+        email: {
+          allowNull: false,
+          type: DataTypes.STRING,
+          unique: "email"
+        },
+        password: {
+          allowNull: false,
+          type: DataTypes.STRING
+        },
+        fonction: {
+          allowNull: true,
+          type: DataTypes.STRING
+        },
+        pseudo: {
+          allowNull: false,
+          type: DataTypes.STRING,
+          unique: "pseudo"
+        },
+        avatar: {
+          allowNull: true,
+          type: DataTypes.TEXT
+        },
+        isAdmin: {
+          allowNull: false,
+          type: DataTypes.BOOLEAN
+        },
+        createdAt: {
+          allowNull: false,
+          type: DataTypes.DATE
+        },
+        updatedAt: {
+          allowNull: false,
+          type: DataTypes.DATE
+        }
+      }, {
+        sequelize,
+        tableName: "users",
+        indexes: [
+          {name: "PRIMARY",
+          unique: true,
+          using: "BTREE",
+          fields: [{ name: "id" }],
+          },
+          {
+            name: "email",
+            unique: true,
+            using: "BTREE",
+            fields: [{ name: "email" }],
+          },
+          {
+            name: "pseudo",
+            unique: true,
+            using: "BTREE",
+            fields: [{ name: "pseudo" }],
+          },
+        ]
+      }
+    );
+    return users
+}
+// User.init({
+//   id: {
+//     allowNull: false,
+//     autoIncrement: true,
+//     primaryKey: true,
+//     type: DataTypes.INTEGER
+//   },
+//   nom: {
+//     allowNull: false,
+//     type: DataTypes.STRING
+//   },
+//   prenom: {
+//     allowNull: false,
+//     type: DataTypes.STRING
+//   },
+//   email: {
+//     allowNull: false,
+//     type: DataTypes.STRING,
+//     unique: "email"
+//   },
+//   password: {
+//     allowNull: false,
+//     type: DataTypes.STRING
+//   },
+//   fonction: {
+//     allowNull: true,
+//     type: DataTypes.STRING
+//   },
+//   pseudo: {
+//     allowNull: false,
+//     type: DataTypes.STRING,
+//     unique: "pseudo"
+//   },
+//   avatar: {
+//     allowNull: true,
+//     type: DataTypes.TEXT
+//   },
+//   isAdmin: {
+//     allowNull: false,
+//     type: DataTypes.BOOLEAN
+//   },
+//   createdAt: {
+//     allowNull: false,
+//     type: DataTypes.DATE
+//   },
+//   updatedAt: {
+//     allowNull: false,
+//     type: DataTypes.DATE
+//   }
+// }, {
+//   sequelize,
+//   modelName: 'User',
+// });
+// module.exports = (sequelize, DataTypes) => {
+//   class Users extends Model {
+//     /**
+//      * Helper method for defining associations.
+//      * This method is not a part of Sequelize lifecycle.
+//      * The `models/index` file will call this method automatically.
+//      */
+//     static associate(models) {
+//       // define association here
+//     }
+//   };
+//   Users.init({
+//     id: {
+//       allowNull: false,
+//       autoIncrement: true,
+//       primaryKey: true,
+//       type: DataTypes.INTEGER
+//     },
+//     nom: {
+//       allowNull: false,
+//       type: DataTypes.STRING
+//     },
+//     prenom: {
+//       allowNull: false,
+//       type: DataTypes.STRING
+//     },
+//     email: {
+//       allowNull: false,
+//       type: DataTypes.STRING,
+//       unique: "email"
+//     },
+//     password: {
+//       allowNull: false,
+//       type: DataTypes.STRING
+//     },
+//     fonction: {
+//       allowNull: true,
+//       type: DataTypes.STRING
+//     },
+//     pseudo: {
+//       allowNull: false,
+//       type: DataTypes.STRING,
+//       unique: "pseudo"
+//     },
+//     avatar: {
+//       allowNull: true,
+//       type: DataTypes.TEXT
+//     },
+//     isAdmin: {
+//       allowNull: false,
+//       type: DataTypes.BOOLEAN
+//     },
+//     createdAt: {
+//       allowNull: false,
+//       type: DataTypes.DATE
+//     },
+//     updatedAt: {
+//       allowNull: false,
+//       type: DataTypes.DATE
+//     }
+//   }, {
+//     sequelize,
+//     modelName: 'Users',
+//   });
+//   return Users;
+// };
+
+// Test créer un user 
+// (async () => {
+//   await User.sync();
+//   // Code here
+//   const user1 = Users.create({
+//   nom: "Chi",
+//   prenom: "Le",
+//   email: "le.chi@yahoo.com",
+//   password:"0123456789",
+//   pseudo: "lechi",
+//   fonction: "",
+//   avatar: "",
+//   isAdmin: "T",
+// });
+// console.log(user1)
+// })();
+
