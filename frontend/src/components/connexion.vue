be7647e7d4e27c7a1d9f992d1600dde7dbf0a1fb
Le Chi <le.khanhchi@yahoo.fr>
2021-03-26 21:23:57 +0100

    changer pour vee-validate, encore erreur

--- frontend/src/components/connexion.vue
+++ frontend/src/components/connexion.vue
@@ -53,54 +53,36 @@
                 <h5 class="pink pb-5 fw-bold fs-3 m-auto text-center">Inscription</h5>
 
                 <form class="form-group  mt-5 mb-5 col col-sm-8 col-md-6 col-lg-4 m-auto text-center" method="post" enctype="multipart/form-data" action="/signup" @submit.prevent = "inscriptionSubmit">
+
                     <div class="form-group row">
-                        <input type="text"  id="nom" name="nom" placeholder="nom" v-model="userForm.nom" required pattern="[A-Za-z][A-Za-z' -]+" class="form-control" 
-                            :class="{ 'is-invalid': isSubmitted && $v.userForm.nom.$error }"/>
-                        <div v-if="isSubmitted && !$v.userForm.name.required" class="invalid-feedback">Veuillez remplir votre nom</div>
+                        <input type="text"  id="nom" name="nom" placeholder="nom" v-model="nom" required pattern="[A-Za-z][A-Za-z' -]+" class="form-control"/>
                     </div>
 
                     <div class="form-group row ">
-                        <input type="text"  id="prenom" name="prenom" v-model="userForm.prenom" placeholder="prénom" required pattern="[A-Za-z][A-Za-z' -]+" class="form-control"
-                            :class="{ 'is-invalid': isSubmitted && $v.userForm.prenom.$error }"/>
-                        <div v-if="isSubmitted && !$v.userForm.prenom.required" class="invalid-feedback">Veuillez remplir votre prénom</div>
+                        <input type="text"  id="prenom" name="prenom" v-model="prenom" placeholder="prénom" required pattern="[A-Za-z][A-Za-z' -]+" class="form-control"/>
                     </div>
 
                     <div class="form-group row ">
-                        <input type="text" id="pseudo" name="pseudo" v-model="userForm.pseudo" placeholder="Votre pseudo" required class="form-control"
-                            :class="{ 'is-invalid': isSubmitted && $v.userForm.pseudo.$error }"/>
-                        <div v-if="isSubmitted && !$v.userForm.pseudo.required" class="invalid-feedback">Veuillez remplir votre pseudo</div>
+                        <input type="text" id="pseudo" name="pseudo" v-model="pseudo" placeholder="Votre pseudo" required class="form-control"/>
                     </div>
 
                     <div class="form-group row ">
-                        <input type="text"  id="fonction" name="fonction" v-model="userForm.fonction" placeholder="fonction" pattern="[A-Za-z][A-Za-z' -]+" class="form-control"/>  
+                        <input type="text"  id="fonction" name="fonction" v-model="fonction" placeholder="fonction" pattern="[A-Za-z][A-Za-z' -]+" class="form-control"/>  
                     </div>
 
                     <div class="form-group row ">
-                        <input type="email"  id="email" name="email" v-model="userForm.email" placeholder="email" required class="form-control"
-                            :class="{ 'is-invalid': isSubmitted && $v.userForm.email.$error }" />
-                        <div v-if="isSubmitted && $v.userForm.email.$error" class="invalid-feedback">
-                            <span v-if="!$v.userForm.email.required">Email est demandé</span>
-                            <span v-if="!$v.userForm.email.email">Invalid email</span>
-                        </div>
+                        <input type="email"  id="email" name="email" v-model="email" placeholder="email" required class="form-control"/>
+                        <span>{{emailError}}</span>
                     </div>
 
                     <div class="form-group row ">
-                        <input type="password" id="password" name="password" v-model="userForm.password" placeholder="password" required class="form-control"
-                            :class="{ 'is-invalid': isSubmitted && $v.userForm.password.$error }"/>
-                        <div v-if="isSubmitted && $v.userForm.password.$error" class="invalid-feedback">
-                            <span v-if="!$v.userForm.password.required">Veuillez remplir votre password</span>
-                            <span v-if="!$v.userForm.password.minLength">Password doit avoir au moins 8 charactères</span>
-                            <span v-if="!$v.userForm.password.maxLength">Password ne peut pas dépasser 20 charactères</span>
-                        </div>
+                        <input type="password" id="password" name="password" v-model="password" placeholder="password" required class="form-control"/>
+                        <span>{{passwordError}}</span>
                     </div>
 
                     <div class="form-group row ">
-                        <input type="password" id="passwordCheck" name="passwordCheck" v-model="userForm.passwordCheck" placeholder="confirmer password" required class="form-control"
-                            :class="{ 'is-invalid': isSubmitted && $v.userForm.passwordCheck.$error }" />
-                        <div v-if="isSubmitted && $v.userForm.passwordCheck.$error" class="invalid-feedback">
-                            <span v-if="!$v.userForm.passwordCheck.required">Veuillez confirmer votre password</span>
-                            <span v-else-if="!$v.userForm.passwordCheck.sameAsPassword">Votre mot de passe doit être le même pour les 2 champs </span>
-                        </div>
+                        <input type="password" id="passwordCheck" name="passwordCheck" v-model="passwordCheck" placeholder="confirmer password" required class="form-control"/>
+                        <span>{{passwordCheckError}}</span>
                     </div>
             
                     <div class="form-group row ">
@@ -111,15 +93,11 @@
                     </div>
 
                      <div class="form-group form-check">
-                        <input type="checkbox" v-model="userForm.accept" @change="$v.userForm.accept.$touch()" id="accept" class="form-check-input">
-                        <label class="form-check-label" :class="{ 'is-invalid': isSubmitted && $v.userForm.accept.$error }" for="accept">Accept terms &nbsp; conditions</label>
-
-                        <div v-if="isSubmitted && $v.userForm.accept.$error" class="invalid-feedback">
-                            <span v-if="!$v.userForm.accept.required">Accept terms and conditions</span>
-                        </div>
+                        <input type="checkbox" v-model="accept" id="accept" required class="form-check-input">
+                        <label class="form-check-label" for="accept">Accept terms &nbsp; conditions</label>
                     </div>
 
-                    <button type="submit" class="btn btn-primary text-center mb-5" id="button" @submit="inscriptionSubmit">Valider</button>
+                    <button type="submit" class="btn btn-primary text-center mb-5" id="button" >Valider</button>
             
                 </form>
 
@@ -131,18 +109,9 @@
 
 <script>
 import Logo from '../components/Logo.vue';
-// import { validationMixin } from 'vuelidate'
-import {
-        required,
-        email,
-        minLength,
-        maxLength,
-        sameAs
-    } from "vuelidate/lib/validators";
-// var Component = Vue.extend({
-//     mixins: [validationMixin],
-//     validations: {}
-// })
+import {  useField, useForm } from 'vee-validate';
+import * as yup from 'yup';
+
 export default {
     name: "connexion",
     components: {
@@ -150,53 +119,52 @@ export default {
     },
     data () {
         return {
-            userForm: {
-                nom:'',
-                prenom:'',
-                email:'',
-                pseudo:'',
-                fonction:'',
-                password:'',
-                passwordCheck:'',
-                avatar:'',
-                // errors: [],
-                accept: ""
-            },
+            nom:'',
+            prenom:'',
+            email:'',
+            pseudo:'',
+            fonction:'',
+            password:'',
+            passwordCheck:'',
+            avatar:'',
+            // errors: [],
             toggle: false,
-            isSubmitted: false
         }
     },
-    validations: {
-            userForm: {
-                nom: {
-                    required
-                },
-                prenom: {
-                    required
-                },
-                pseudo: {
-                    required
-                },
-                email: {
-                    required,
-                    email
-                },
-                password: {
-                    required,
-                    minLength: minLength(8),
-                    maxLength: maxLength(20)
-                },
-                confirmPassword: {
-                    required,
-                    sameAsPassword: sameAs('password')
-                },
-                accept: {
-                    required (val) {
-                      return val
-                    }
-                }
-            }
-        },
+    setup () {
+        // Define a validation schema
+        const schema = yup.object({
+        email: yup.string()
+                .required('Veuillez remplir votre email')
+                .email('Email invalid'),
+        password: yup.string()
+                .min(8, 'Password doit avoir au minimum 8 characters')
+                .max(20, 'Password doit avoir au maximum 20 characters')
+                .required('Password is required'),
+        passwordCheck: yup.string()
+                .oneOf([yup.ref('password'), null], 'Passwords must match')
+                .required('Confirm Password is required'),
+        });
+
+    // Create a form context with the validation schema
+        useForm({
+        validationSchema: schema,
+        });
+        // No need to define rules for fields
+        const { value: email, errorMessage: emailError } = useField('email');
+        const { value: password, errorMessage: passwordError } = useField('password');
+        const { value: passwordCheck, errorMessage: passwordCheckError } = useField('passwordCheck');
+
+        return {
+        email,
+        emailError,
+        password,
+        passwordError,
+        passwordCheck,
+        passwordCheckError
+        };
+    },
+
     methods: {
         // fonction pour switcher entre template login et signup
         switchToggle () {
@@ -226,14 +194,14 @@ export default {
             //     this.errors.push("Password doit comprendre entre 8 et 20 charactères")
             // }
             // si tout est OK, créer user
-            
+
                 let user = {
-                    nom: this.userForm.nom,
-                    prenom: this.userForm.prenom,
-                    email: this.userForm.email,
-                    password: this.userForm.password,
-                    fonction: this.userForm.fonction,
-                    avatar: this.userForm.avatar
+                    nom: this.nom,
+                    prenom: this.prenom,
+                    email: this.email,
+                    password: this.password,
+                    fonction: this.fonction,
+                    avatar: this.avatar
                     };
                 let form = new FormData();
                 form.append("user", JSON.stringify(user));
@@ -268,12 +236,13 @@ export default {
         loadAvatar(e) {
             let error_file = document.getElementById('error_file');
             let avatar = document.getElementById('avatar').files[0];
-            if(avatar.type!==("png" ||"jpg" || "jpeg")) { 
-                e.stopPropagation();
+            if(this.avatar.type!=("png" ||"jpg" || "jpeg")) { 
+                e.preventDefault();
                 error_file.innerHTML = "Veuillez choisir le bon format de l'image"
             }
             else { 
-                this.avatar = avatar
+                this.avatar = avatar;
+                error_file.innerHTML=""
             }
         }
     }
