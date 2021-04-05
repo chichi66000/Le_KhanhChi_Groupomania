8674216842f647514c08d44166f9a536d3214cc0
Le Chi <le.khanhchi@yahoo.fr>
2021-04-05 08:57:03 +0200

    modifier navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -10,8 +10,20 @@
                 </button>
 
                 <div id="navbarContent" class="collapse navbar-collapse col col-sm-8 col-md-8 col-lg-8 mx-auto text-center font-weight-bolder justify-self-center dropdown-menu-lg-right justify-content-between text-light">
-                    <router-link to="/login">Login</router-link>
-                    <router-link to="/signup">Signup</router-link>
+                    <div class=" ">
+                        <router-link to="/home">HOME</router-link>
+                    </div>
+                    <div class=" collape navbar-collapse ">
+                        <ul class="navbar-nav ml-auto">
+                            <li class="nav-item px-5">
+                                <router-link to="/login">Login</router-link>
+                            </li>
+                            <li class="nav-item px-5">
+                                <router-link to="/signup">Signup</router-link>
+                            </li>
+                        </ul>
+
+                    </div>
 
                     <!-- <router-link to="/">Home  </router-link> 
                     <router-link to="/help">FAQ  </router-link> 
