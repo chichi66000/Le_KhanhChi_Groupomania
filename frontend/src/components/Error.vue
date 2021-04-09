e2b8763bd79489e7651c95ab6fb469f1cf19d8ac
Le Chi <le.khanhchi@yahoo.fr>
2021-04-09 10:17:09 +0200

    images avatar

---
+++ frontend/src/components/Error.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="alert alert-danger mx-auto mt-5 mb-5" role="alert">
+        {{error}}
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Error",
+    props: [ 'error']
+}
+</script>
