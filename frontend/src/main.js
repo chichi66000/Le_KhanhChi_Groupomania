3a8a5b255c1ed8f2d25cea08720f2811d648b63a
Le Chi <le.khanhchi@yahoo.fr>
2021-04-21 11:26:29 +0200

    regler axios dans main.js pour ne pas avoir pb de bearer null de temps en temps

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,13 +4,13 @@ import { createApp} from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-
+import axios from 'axios'
 // modules pour bootstrap
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import '../src/assets/custom.scss'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router, axios).mount('#app')
 
 import jQuery from 'jquery'
 window.jQuery = window.$ = jQuery
