fa7b7cb627b088c4a303fa996802dc6776d39633
Le Chi <le.khanhchi@yahoo.fr>
2021-04-24 17:19:23 +0200

    route getUserPost testé coté backend

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -11,11 +11,14 @@ router.post('/', auth, multer, postCtrl.createPost)
 // récupérer tous les publications                          // OK, testé
 router.get('/', auth, postCtrl.getAllPosts)
 
-//====> Misa à jour publication <====\\
+//====> Misa à jour publication <====\\                     // OK testé
 router.put("/:postId/:id/update", auth, multer, postCtrl.updatePost);
 
 //====> Suppression d'une publication <====\\               // OK testé
 router.delete("/:postId", auth, postCtrl.deletePost);
+
+//====> Récupèration des publications de l'utilisateur <====\\      // OK testé
+router.get("/:id", auth, postCtrl.getUserPosts);
 
 //====> Ajout ou suppresson de like <====\\                 // OK testé
 router.post("/:postId/:userId/like", auth, postCtrl.createLike);
