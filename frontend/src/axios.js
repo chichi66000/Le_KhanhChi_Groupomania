2588e3b57b8f77ac180b7e5cd1651d90ecf8b85b
Le Chi <le.khanhchi@yahoo.fr>
2021-05-13 22:18:38 +0200

    réussir à relancer la requete avec refreshtoken

--- frontend/src/axios.js
+++ frontend/src/axios.js
@@ -4,6 +4,7 @@ var tough = require('tough-cookie');
 var cookiejar = new tough.CookieJar();
 // import { refreshToken } from "../../backend/controllers/user"
 // import router from './router/index'
+// import createAuthRefreshInterceptor from 'axios-auth-refresh';
 
 const axiosInstance = axios.create({
     baseURL: "http://localhost:5000/" ,
@@ -16,35 +17,44 @@ const axiosInstance = axios.create({
     }
     });
 
+// Add a response interceptor
+axiosInstance.interceptors.response.use (
+    (response) => {
+        console.log(response)
+        return response;
+    },
+    (error) => {        // si error 
+       
+        console.log({error})
+        const id = localStorage.getItem('id')
+        const originalRequest = error.config;
+        originalRequest._retry = true;
+        const status = error.response ? error.response.status : null;
+        // si le status = 401 , unauthorization
+        if (status === 401) {
+            originalRequest._retry = true;
+            // envoyer au server le cookie refreshToken pour demander 1 nouveau token
+            
+            return axiosInstance.post(`http://localhost:5000/api/auth/refresh/${id}`, { withCredentials: true, credentials: 'include'}
+            )
+                .then( response => {
+                    // recevoir le token du server, et stocker dans localstorage pour authentifier 
+                    if(response.status === 201) {
+                        console.log('newtoken ', response.data )
+                        localStorage.setItem('token', response.data);
+                        axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
+                        return axiosInstance(originalRequest);
+                    }
+                })
+                .catch( e => {
+                    console.log(e)
+                })
+        }
+        return Promise.reject(error);
+    }
+)
 
-    // function saveToken(token, refreshToken) {
-    //     localStorage.setItem('token', token);
-    //     this.$cookies.set('refreshToken', refreshToken);
-    //   }
-    //   function destroyToken() {
-    //     localStorage.removeItem('token');
-    //     sessionStorage.remove('refreshToken');
-    //   }
 
-    // funtion pour envoyer la demande de refreshToken
-    // function refresh() {
-    //     const refreshToken = this.$cookies.get('refreshToken')
-    //     console.log({refreshToken})
-    //     return new Promise((resolve, reject) => {
-    //       axios.post('/api/auth/refresh',{ 
-    //         refreshToken,
-    //         credentials: 'include',
-    //     }
-    //       ).then((response) => {
-    //         saveToken(response.data.token, response.data.refreshToken);
-    //         return resolve(response.data.token);
-    //       }).catch((error) => {
-    //         destroyToken();
-    //         window.location.replace('/logout');
-    //         return reject(error);
-    //       });
-    //     });
-    //   }
 // Add a request interceptor
 axiosInstance.interceptors.request.use(
     // function config
@@ -70,42 +80,6 @@ axiosInstance.interceptors.request.use(
     // function error
     error => {
         Promise.reject(error)
-    }
-)
-
-// Add a response interceptor
-axiosInstance.interceptors.response.use (
-    (response) => {
-        console.log(response)
-        return response;
-    },
-     (error) => {        // si error 
-        console.log({error})
-        const id = localStorage.getItem('id')
-        const originalRequest = error.config;
-        const status = error.response ? error.response.status : null;
-        // si le status = 401 , unauthorization
-        if (status === 401) {
-            originalRequest._retry = true;
-            // envoyer au server le cookie refreshToken pour demander 1 nouveau token
-            
-            return axiosInstance.post(`http://localhost:5000/api/auth/refresh/${id}`, { withCredentials: true, credentials: 'include'}
-            )
-                .then( response => {
-                    // recevoir le token du server, et stocker dans localstorage pour authentifier 
-                    if(response.status === 201) {
-                        console.log('newtoken ', response.data )
-                        localStorage.setItem('token', response.data);
-                        axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
-                        return axios(originalRequest);
-                    }
-                })
-                .catch( e => {
-                    console.log(e)
-                })
-            // refresh()
-        }
-        return Promise.reject(error);
     }
 )
 
